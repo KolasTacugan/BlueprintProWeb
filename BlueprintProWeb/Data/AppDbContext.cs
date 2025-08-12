088620eecafc3faed762adaccdf5ddf7cdc9259f
KolasTacugan <nicoletacuganbsit@gmail.com>
2025-08-12 22:23:37 +0800

    Login entry NOT FINISH

---
+++ BlueprintProWeb/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BlueprintProWeb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlueprintProWeb.Data
+{
+    public class AppDbContext : IdentityDbContext<Client>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        // DbSets for your entities can be added here, e.g.:
+        // public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
+    
---
+++ BlueprintProWeb/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlueprintProWeb.Models
+{
+    public class Client : IdentityUser  
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
