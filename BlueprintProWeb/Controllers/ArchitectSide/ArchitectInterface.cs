f43bceb925850bee273b49e7293a19d3ce53c6a2
Gene Harold <geneisme20@gmail.com>
2025-08-19 22:57:03 +0800

    SHARED LAYOUT FOR DASHBOARDS

---
+++ BlueprintProWeb/Controllers/ArchitectSide/ArchitectInterface.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlueprintProWeb.Controllers.ArchitectSide
+{
+    public class ArchitectInterface : Controller
+    {
+        public IActionResult ArchitectDashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlueprintProWeb/Controllers/ClientSide/ClientInterface.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlueprintProWeb.Controllers.ClientSide
+{
+    public class ClientInterface : Controller
+    {
+        public IActionResult ClientDashboard()
+        {
+            return View();
+        }
+    }
+}
