0e2a4f3ccb1c5c978e5d35405891d34b9a1f52d4
KolasTacugan <nicoletacuganbsit@gmail.com>
2025-08-28 01:21:47 +0800

    MARKETPLACE UNFINISH HEHE

---
+++ BlueprintProWeb/Controllers/MarketplaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlueprintProWeb.Controllers
+{
+    public class MarketplaceController : Controller
+    {
+        public IActionResult BlueprintMarketplace()
+        {
+            return View();
+        }
+    }
+}
