7eb19119365b45178ccd1501a50cafb7bef723fa
Gene Harold <geneisme20@gmail.com>
2025-09-09 20:35:06 +0800

    MIGRATIONS

---
+++ BlueprintProWeb/Migrations/20250909114538_UpdateBPTableArchiId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlueprintProWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBPTableArchiId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "architectId",
+                table: "Blueprints",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "architectId",
+                table: "Blueprints");
+        }
+    }
+}
