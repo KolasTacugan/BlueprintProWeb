7456923a2b5e4f2f5e4f852b82adb986daeb8151
Gene Harold <geneisme20@gmail.com>
2025-09-09 18:35:06 +0800

    UPDATE BP TABLE

---
+++ BlueprintProWeb/Migrations/20250909072426_UpdateBPTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlueprintProWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBPTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "blueprintIsForSale",
+                table: "Blueprints",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "blueprintIsForSale",
+                table: "Blueprints");
+        }
+    }
+}
