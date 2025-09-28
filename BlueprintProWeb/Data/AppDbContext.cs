bd1894f9b322fe85f1eac3162f88b759438c9b56
Gene Harold <geneisme20@gmail.com>
2025-09-28 16:35:40 +0800

    PROJECT TRACK RELATED ENTITIES ADDED

--- BlueprintProWeb/Data/AppDbContext.cs
+++ BlueprintProWeb/Data/AppDbContext.cs
@@ -15,6 +15,11 @@ namespace BlueprintProWeb.Data
         public DbSet<Match> Matches { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<ProjectTracker> ProjectTrackers { get; set; }
+        public DbSet<Compliance> Compliances { get; set; }
+        public DbSet<ProjectFile> ProjectFiles { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -53,6 +58,42 @@ namespace BlueprintProWeb.Data
                       .HasForeignKey(p => p.blueprint_Id)
                       .OnDelete(DeleteBehavior.Cascade);
             });
+
+            builder.Entity<Message>()
+                .HasOne(m => m.Client)
+                .WithMany()
+                .HasForeignKey(m => m.ClientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(m => m.Architect)
+                .WithMany()
+                .HasForeignKey(m => m.ArchitectId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(m => m.Sender)
+                .WithMany()
+                .HasForeignKey(m => m.SenderId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<ProjectTracker>()
+                .HasOne(pt => pt.Project)
+                .WithOne()
+                .HasForeignKey<ProjectTracker>(pt => pt.project_Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Compliance>()
+                .HasOne(c => c.ProjectTracker)
+                .WithOne(pt => pt.Compliance)
+                .HasForeignKey<Compliance>(c => c.projectTrack_Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<ProjectFile>()
+                .HasOne(pf => pf.Project)
+                .WithMany()
+                .HasForeignKey(pf => pf.project_Id)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
---
+++ BlueprintProWeb/Models/ProjectFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlueprintProWeb.Models
+{
+    public class ProjectFile
+    {
+        [Key]
+        public int projectFile_Id { get; set; }
+
+        // FK → Project
+        [Required]
+        public string project_Id { get; set; }
+        public Project Project { get; set; }
+
+        // File details
+        [Required]
+        public string projectFile_fileName { get; set; }
+
+        [Required]
+        public string projectFile_Path { get; set; }
+
+        public int projectFile_Version { get; set; }   // increments per project_Id
+        public DateTime projectFile_uploadedDate { get; set; } = DateTime.UtcNow;
+    }
+}
