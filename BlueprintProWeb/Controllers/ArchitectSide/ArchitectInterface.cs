72be1f4036010fddc132880615a478fe48ba4422
Gene Harold <geneisme20@gmail.com>
2025-12-01 13:10:45 +0800

    MINOR CHANGES

--- BlueprintProWeb/Controllers/ArchitectSide/ArchitectInterface.cs
+++ BlueprintProWeb/Controllers/ArchitectSide/ArchitectInterface.cs
@@ -516,7 +516,7 @@ namespace BlueprintProWeb.Controllers.ArchitectSide
                     MatchStatus = m.MatchStatus,
                     MatchDate = m.MatchDate,
                     ClientProfileUrl = string.IsNullOrEmpty(m.Client.user_profilePhoto)
-                        ? "/images/default-profile.png"
+                        ? "/images/profile.jpg"
                         : m.Client.user_profilePhoto
                 })
                 .ToListAsync();
